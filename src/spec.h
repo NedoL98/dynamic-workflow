038713921908a0a69a3cada631ab37a176097cc2
NedoL98 <a.nedol@yandex.ru>
2019-02-24 22:17:56 +0300

    Removing VMSpec

--- src/spec.h
+++ src/spec.h
@@ -12,7 +12,7 @@ struct TaskSpec {
 class VMDescription {
 public:
     VMDescription() = default;
-    VMDescription(int cores, double memory, double flops, int price);
+    VMDescription(int id, int cores, double memory, double flops, int price);
 
     int GetCores() const;
     double GetMemory() const;
@@ -27,13 +27,9 @@ public:
     bool operator >=(const VMDescription& other) const;
 
 private:
+    int Id;
     int Cores;
     double Memory;
     double Flops;
     int Price;
-};
-
-struct VMSpec {
-    VMDescription properties;
-    int id;
 };
