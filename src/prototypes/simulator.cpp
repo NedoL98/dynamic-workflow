5e7e8587e2bc3abf5fb3cff1daf8ce230294a64e
NedoL98 <a.nedol@yandex.ru>
2019-04-18 13:13:27 +0300

    Fixing print format

--- src/prototypes/simulator.cpp
+++ src/prototypes/simulator.cpp
@@ -84,7 +84,7 @@ void CloudSimulator::Run(double timeout) {
 
 bool CloudSimulator::RegisterVirtualMachine(const VMDescription &stats, int customId) {
     int hostId = -1;
-    XBT_DEBUG("%d %lld %f", stats.GetCores(), stats.GetFlops(), stats.GetMemory());
+    XBT_DEBUG("%d %lld %lld", stats.GetCores(), stats.GetFlops(), stats.GetMemory());
     if ((hostId = Platform.GetEmptyHost(stats.GetSpec())) == -1) {
         XBT_INFO("No empty hosts");
         return false;
